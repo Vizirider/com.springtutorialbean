c2ac974892310419b3b8b48e5dd8fbf99ec99211
Robert Vizi <vizirider@gmail.com>
2020-06-08 17:36:02 +0200

    sql features
    sql features based on tutorial

--- src/main/java/com/springtutorialbean/App.java
+++ src/main/java/com/springtutorialbean/App.java
@@ -1,10 +1,12 @@
 package com.springtutorialbean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 public class App {
 	
@@ -13,26 +15,49 @@ public class App {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springtutorialbean/beans/beans.xml");
 		
-		Person person = (Person)context.getBean("person");
+		//Person person = (Person)context.getBean("person");
 		
 		OffersDAO offersDao = (OffersDAO)context.getBean("offersDao");
 		
 		try {
 			
+			List<Offer> offersList = new ArrayList<Offer>();
+			
+			offersList.add(new Offer(1, "Vizirider", "vizirider@gmail.com", "Elegent web design"));
+			
+			int[] rvals = offersDao.create(offersList);
+			
+			for(int value: rvals)
+			{
+				System.out.println("Updated:" + value + "rows");
+			}
+			
+			Offer offer1 = new Offer("kate", "kate@hobby.local", "coding is awesome");
+			
+			if(offersDao.create(offer1)) {
+				System.out.println("Created offer object");
+			}
+			offersDao.delete(3);
 			List<Offer> offers = offersDao.getOffers();
 			
 			for(Offer offer: offers) {
 				System.out.println(offer);
-			}			
+			}
 			
-		} catch (DataAccessException ex) {
+			Offer offer = offersDao.getOffersbyid(2);
+			System.out.println("Should be john: " + offer);
+			
+		} catch (CannotGetJdbcConnectionException ex) {
+			System.out.println("Cannot get database connection");
+		}
+		catch (DataAccessException ex) {
 			System.out.println(ex.getMessage());
 			System.out.println(ex.getClass());
 		}
 		
 
 		
-		System.out.println(person);
+		//System.out.println(person);
 		
 		//Address address2 = (Address)context.getBean("address2");
 		//System.out.println(address2);
--- src/main/java/com/springtutorialbean/OffersDAO.java
+++ src/main/java/com/springtutorialbean/OffersDAO.java
@@ -9,21 +9,32 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component("offersDao")
 public class OffersDAO {
 
-	private JdbcTemplate jdbc;
+	private NamedParameterJdbcTemplate jdbc;
 	
 	@Autowired
 	public void setDataSource(DataSource jdbc) {
-		this.jdbc = new JdbcTemplate(jdbc);
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
 	}
 
-	public List<Offer> getOffers(){
+	public List<Offer> getOffersbyname(){
 		
-		return jdbc.query("select * from offers", new RowMapper<Offer>() {
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("name", "john");
+		
+		return jdbc.query("select * from offers where name =:name", params, new RowMapper<Offer>() {
 
 			public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
 				Offer offer = new Offer();
@@ -37,5 +48,68 @@ public class OffersDAO {
 			}
 			
 		});	
+	}
+	
+	public Offer getOffersbyid(int id){
+		
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("id", id);
+		
+		return jdbc.queryForObject("select * from offers where id =:id", params, 
+				new RowMapper<Offer>() {
+
+					public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+						Offer offer = new Offer();
+
+						offer.setId(rs.getInt("id"));
+						offer.setName(rs.getString("name"));
+						offer.setEmail(rs.getString("email"));
+						offer.setText(rs.getString("text"));
+
+						return offer;
+					}
+			
+				});	
+	}
+	
+	public List<Offer> getOffers(){
+			
+			return jdbc.query("select * from offers", new RowMapper<Offer>() {
+	
+				public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+					Offer offer = new Offer();
+					
+					offer.setId(rs.getInt("id"));
+					offer.setName(rs.getString("name"));
+					offer.setEmail(rs.getString("email"));
+					offer.setText(rs.getString("text"));
+					
+					return offer;
+				}
+				
+			});	
+	}
+	
+	public boolean delete(int id) {
+		MapSqlParameterSource params = new MapSqlParameterSource("id", id);
+
+		return jdbc.update("delete from offers where id=:id", params) == 1;
+	}
+	
+	public boolean create(Offer offer) {
+		
+		BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(offer);
+		
+		return jdbc.update("insert into offers (name, text, email) values (:name, :text, :email)", params) == 1;
+	}
+	
+	@Transactional(isolation = Isolation.DEFAULT)
+	public int[] create(List<Offer> offers) {
+		
+		
+		SqlParameterSource[] params = SqlParameterSourceUtils.createBatch(offers.toArray());
+		
+		return jdbc.batchUpdate("insert into offers (id, name, text, email) values (:id, :name, :text, :email)", params);
+
 	}
 }
